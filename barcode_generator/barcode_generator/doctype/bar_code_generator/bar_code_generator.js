087faaa0ae516edf95e4809687bd4476f89eb1b2
deep <deep@sanskartechnolab.com>
2024-09-13 11:13:49 +0530

    js-added

--- barcode_generator/barcode_generator/doctype/bar_code_generator/bar_code_generator.js
+++ barcode_generator/barcode_generator/doctype/bar_code_generator/bar_code_generator.js
@@ -6,3 +6,57 @@
 
 // 	},
 // });
+frappe.ui.form.on("Bar-code Generator", {
+    get_serial_no(frm) {
+        frappe.call({
+            method: "barcode_generator.barcode_generator.doctype.bar_code_generator.bar_code_generator.get_serial_no",
+            args: {
+                item_code: frm.doc.item_code,
+                limit: frm.doc.number_of_barcode
+            },
+            callback: function (r) {
+                var serialNumbers = r.message;
+                console.log(serialNumbers)
+                if (serialNumbers.length != frm.doc.number_of_barcode) {
+                    frm.set_value("details", 0)
+                    frappe.msgprint("Requested Serial No: " + frm.doc.number_of_barcode + ". Available Serial Nos: " + serialNumbers.length + ". Please adjust the requested quantity for generating barcodes accordingly.")
+                    frm.set_value("number_of_barcode", undefined)
+                }
+                else {
+                    frm.set_value("details", 0)
+                    serialNumbers.forEach(function (obj) {
+                        var child_create = frm.add_child("details");
+                        child_create.serial_no = obj['name'];
+                        child_create.item_name = obj['item_name'];
+                    })
+                }
+            },
+        });
+    },
+    on_submit: function (frm) {
+        frappe.call({
+            method: "barcode_generator.barcode_generator.doctype.bar_code_generator.bar_code_generator.set_status",
+            args: {
+                nm: frm.doc.details
+            }
+        })
+    },
+    after_cancel: function (frm) {
+        frappe.call({
+            method: "barcode_generator.barcode_generator.doctype.bar_code_generator.bar_code_generator.set_status_untick",
+            args: {
+                nm: frm.doc.details
+            }
+        })
+    },
+    onload:function(frm){
+        cur_frm.set_query("item_code", function() {
+            return {
+                "filters": {
+                    "has_serial_no": 1,
+                    // "name": ["not in", "Services"],
+                }
+            };
+        });
+    }
+});
--- barcode_generator/barcode_generator/doctype/master_barcode_generator/master_barcode_generator.js
+++ barcode_generator/barcode_generator/doctype/master_barcode_generator/master_barcode_generator.js
@@ -6,3 +6,58 @@
 
 // 	},
 // });
+frappe.ui.form.on("Master Barcode Generator", {
+    get_serial_no(frm) {
+        frappe.call({
+            method: "barcode_generator.barcode_generator.doctype.master_barcode_generator.master_barcode_generator.get_serial_no",
+            args: {
+                item_code: frm.doc.item_code,
+                limit: frm.doc.qty
+            },
+            callback: function (r) {
+                var serialNumbers = r.message;
+                console.log(serialNumbers.length)
+                if (serialNumbers.length != frm.doc.qty) {
+                    
+                    frm.set_value("master_barcode_details", 0)
+                    frappe.msgprint("Requested Serial No: " + frm.doc.qty + ". Available Serial Nos: " + serialNumbers.length + ". Please adjust the requested quantity for generating barcodes accordingly.")
+                    frm.set_value("qty", undefined)
+                }
+                else{
+                    frm.set_value("master_barcode_details", 0)
+                    serialNumbers.forEach(function (obj) {
+                        var child_create = frm.add_child("master_barcode_details");
+                        child_create.serial_no = obj['name'];
+                        child_create.item_name = obj['item_name'];
+                    })
+                }
+            },
+        });
+    },
+    on_submit: function (frm) {
+        frappe.call({
+            method: "barcode_generator.barcode_generator.doctype.master_barcode_generator.master_barcode_generator.set_status",
+            args: {
+                nm: frm.doc.master_barcode_details
+            }
+        })
+    },
+    after_cancel: function (frm) {
+        frappe.call({
+            method: "barcode_generator.barcode_generator.doctype.master_barcode_generator.master_barcode_generator.set_status_untick",
+            args: {
+                nm: frm.doc.master_barcode_details
+            }
+        })
+    },
+    onload:function(frm){
+        cur_frm.set_query("item_code", function() {
+            return {
+                "filters": {
+                    "has_serial_no": 1,
+                    // "name": ["not in", "Services"],
+                }
+            };
+        });
+    }
+});
